bac63f7c0380c9aebe5deea0207b58a95aee86ec
aalnoman10 <aalnomanm10@gmail.com>
2023-05-11 23:57:07 +0600

    update ui for delete coffee item

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,12 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 const Home = () => {
-    const [data, setData] = useState([])
+    const [coffees, setCoffees] = useState([])
 
     useEffect(() => {
         fetch('http://localhost:5000/coffee')
             .then(res => res.json())
-            .then(data => setData(data))
+            .then(data => setCoffees(data))
     }, [])
 
     // const handleView = (id) => {
@@ -25,11 +25,12 @@ const Home = () => {
                 method: "DELETE"
             })
                 .then(res => res.json())
-                .then(del => {
-                    // const del = data
-                    setData(data)
-                    if (del.deletedCount > 0) {
+                .then(data => {
+                    // setData(data)
+                    if (data.deletedCount > 0) {
                         alert('su')
+                        const remaining = coffees.filter(coffee => coffee._id !== id)
+                        setCoffees(remaining)
                     }
                 })
         }
@@ -44,20 +45,20 @@ const Home = () => {
             </div>
             <div className="grid md:grid-cols-2 gap-4 md:px-44">
                 {
-                    data.map(d => <div key={d._id}>
+                    coffees.map(coffee => <div key={coffee._id}>
                         <div className="flex justify-between items-center bg-slate-300 p-4">
                             <div className='w-1/3'>
-                                <img src={d?.photo} alt="" />
+                                <img src={coffee?.photo} alt="" />
                             </div>
                             <div>
-                                <p><b className='font-medium'>Name : </b>{d.name}</p>
-                                <p><b className='font-medium'>Chef : </b>{d.chef}</p>
-                                <p><b className='font-medium'>Price : </b>{d.price}</p>
+                                <p><b className='font-medium'>Name : </b>{coffee.name}</p>
+                                <p><b className='font-medium'>Chef : </b>{coffee.chef}</p>
+                                <p><b className='font-medium'>Price : </b>{coffee.price}</p>
                             </div>
                             <div>
                                 <p><button className='bg-orange-300 p-2 w-8'>W</button></p>
-                                <p><Link to={`/updateCoffee/${d._id}`}><button className='bg-slate-800 italic p-2 w-8'>l</button></Link></p>
-                                <p><button onClick={() => handleDelete(d._id)} className='bg-red-400 p-2 w-8'>X</button></p>
+                                <p><Link to={`/updateCoffee/${coffee._id}`}><button className='bg-slate-800 italic p-2 w-8'>l</button></Link></p>
+                                <p><button onClick={() => handleDelete(coffee._id)} className='bg-red-400 p-2 w-8'>X</button></p>
                             </div>
                         </div>
                     </div>
